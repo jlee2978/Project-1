82517c664dbae6947aefaa511abaf8a4957d8e44
jlee2978 <35323949+jlee2978@users.noreply.github.com>
2022-04-05 00:06:26 -0400

    added greeting

--- main.js
+++ main.js
@@ -3,8 +3,16 @@
 document.querySelector('#userGreeting').addEventListener('submit', helloUser)
 
 function helloUser() {
-    let user = document.querySelector('#username').value
+    let user = document.querySelector('#username').value;
     // to change from alert to text greeting on page
-    alert('hello '+user);
+    // alert('Hello '+user);
+    
+    let greetingTag = document.createElement("p");
+    greetingTag.innerHTML = "Hello "+user;
+    // greetingText = document.createTextNode("Hello "+user);
+    // greetingTag.appendChild(greetingText);
+    var element = document.getElementById("greeting");
+    element.appendChild(greetingTag);
+
     return false;   // don't submit the form
 }
